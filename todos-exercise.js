1fabc77bb8787186785c6d37f6c71259142fc1e2
jade0304 <stefanie.dn88@gmail.com>
2021-09-01 10:54:49 -0500

    place some code to test jasmin

--- todos-exercise.js
+++ todos-exercise.js
@@ -1,11 +1,29 @@
 function calculateTaxes(income){
-  if (!Number.isFinite(income)){
-    throw new Error ('BAD INCOME');
-  }
-  if (income > 30000){
+  if (income > 30000) {
     return income * 0.25;
   }
   else {
     return income * 0.15;
   }
 }
+
+
+
+
+
+
+
+
+
+
+// function calculateTaxes(income){
+//   if (!Number.isFinite(income)){
+//     throw new Error ('BAD INCOME');
+//   }
+//   if (income > 30000){
+//     return income * 0.25;
+//   }
+//   else {
+//     return income * 0.15;
+//   }
+// }
--- todos-exercise.test.js
+++ todos-exercise.test.js
@@ -1,7 +1,9 @@
-it ('should calculate high-tax bracket', function(){
-  expect(1 + 1)toEqual(2);
-  expect(1 + 1)toEqual(2);
-
-})
+it ('should calculate high-tax bracket', function() {
+  expect(calculateTaxes(50000)).toEqual(12500);
+  expect(calculateTaxes(100000)).toEqual(25000);
+});
 it ('should calculate low-tax bracket', function(){
+  expect(calculateTaxes(10000)).toEqual(1500);
+  expect(calculateTaxes(1000)).toEqual(150);
+  expect(calculateTaxes(0)).toEqual(0);
 })
